90bfe8365e8d6a3a561949fc7f8792cee1a4f358
KeisLuv5991 <chmhj9997@gmail.com>
2021-04-12 00:23:53 +0900

    weekly-report update

--- servers/server.js
+++ servers/server.js
@@ -1,28 +1,53 @@
 
 const express = require('express');
-
 const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
 const port = process.env.PORT || 3001;
 
-
-app.use('/api', (req, res) => res.json(
-    {
-        "problems": [
-          {
-            "id": "1",
-            "name": "Elevator",
-            "explanation" : "엘리베이터 제어 시스템"
-          },
-          {
-            "id": "2",
-            "name": "SNS",
-            "explanation" : "팔로잉 추천을 통해 카카오팔로잉 사용자들의 팔로잉이 각각 20명 이상이 되도록 하는 추천시스템 구현"
-          }
-        ]
-    }
-));
-
-app.listen(port, () => {
-    console.log(`express is running on ${port}`);
+server.listen(port, () => {
+  console.log(`express is running on ${port}`);
 });
+
+
+
+//public directory open
+app.use('/public', express.static('./public'));
+app.get('/', (req, res) => {
+  res.redirect(302, '/public')
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
+
+
+
+
+
+
+let data = {
+  "problems": [
+    {
+      "id": "1",
+      "name": "Elevator",
+      "explanation" : "엘리베이터 제어 시스템"
+    },
+    {
+      "id": "2",
+      "name": "SNS",
+      "explanation" : "팔로잉 추천을 사용자들의 팔로잉이 각각 20명 이상이 되도록 하는 추천시스템 구현"
+    }
+  ]
+};
+
+app.use('/api', (req, res) => res.json(data));
+
 
--- src/pages/Problems.js
+++ src/pages/Problems.js
@@ -7,15 +7,7 @@ class Problems extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            problems: 
-            [
-                {
-                    "id": "0",
-                    "name": "null",
-                    "explanation" : "null"
-                }
-            ]
-            
+            problems: [],
         };
     }
 
@@ -31,16 +23,17 @@ class Problems extends Component {
     }
 
     render() {
+        const { problems } = this.state;
         return (
             <>
                 <h1>Problems</h1>
                 <Route 
                     exact path={this.props.match.path} 
-                    render={(props) => <ProblemList problems={this.state.problems} {...props} />}
+                    render={(props) => <ProblemList problems={problems} {...props} />}
                 />
                 <Route 
                     path={`${this.props.match.path}/:id`} 
-                    render={(props) => <Problem problems={this.state.problems} {...props} />}
+                    render={(props) => <Problem problems={problems} {...props} />}
                 />
 
             </>
